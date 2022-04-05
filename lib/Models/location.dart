ef3d685c3bbe9639fd02ebc28ac4012838dbbbfa
Nahed Oukal <aoox6657@gmail.com>
2022-04-05 13:51:39 +0300

    commit

--- lib/Models/location.dart
+++
@@ -1,9 +0,0 @@
-class Location {
-  const Location(this.lat, this.lng);
-  final double lat;
-  final double lng;
-
-  Location.fromJson(Map<dynamic, dynamic> parsedJson)
-      : lat = parsedJson['lat'],
-        lng = parsedJson['lng'];
-}
